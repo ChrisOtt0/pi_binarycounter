b18834f77c57c5cc1ba8185ac9604c41d8dbd8c1
ChrisOtt0 <sophusott@gmail.com>
2023-01-30 17:29:29 +0100

    Full test

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from modules.binary_wrapper import BinaryWrapper
-import time
+import sys
+
+def close(bw: BinaryWrapper):
+    bw.zero()
+    sys.exit()
 
 port1 = 18
 port2 = 24
@@ -8,6 +12,32 @@ port4 = 16
 
 bw = BinaryWrapper(port1, port2, port3, port4)
 
-bw.test()
-time.sleep(10)
-bw.off()
+functionality = {
+    0: bw.zero,
+    1: bw.one,
+    2: bw.two,
+    3: bw.three,
+    4: bw.four,
+    5: bw.five,
+    6: bw.six,
+    7: bw.seven,
+    8: bw.eight,
+    9: bw.nine,
+    10: bw.ten,
+    11: bw.eleven,
+    12: bw.twelve,
+    13: bw.thirteen,
+    14: bw.fourteen,
+    15: bw.fifteen,
+    16: close
+}
+
+while True:
+    num = 0
+    try:
+        num = int(input("Input a number between 0-15. 16 to exit."))
+        break
+    except:
+        print("Unknown request. Please try again.")
+
+    functionality[num]()
--- modules/binary_wrapper.py
+++ modules/binary_wrapper.py
@@ -24,9 +24,99 @@ class BinaryWrapper:
         self.gpio.output(self.port3, True)
         self.gpio.output(self.port4, True)
 
-    def off(self):
+    def zero(self):
         self.gpio.output(self.port1, False)
         self.gpio.output(self.port2, False)
         self.gpio.output(self.port3, False)
         self.gpio.output(self.port4, False)
+
+    def one(self):
+        self.gpio.output(self.port1, True)
+        self.gpio.output(self.port2, False)
+        self.gpio.output(self.port3, False)
+        self.gpio.output(self.port4, False)
+        
+    def two(self):
+        self.gpio.output(self.port1, False)
+        self.gpio.output(self.port2, True)
+        self.gpio.output(self.port3, False)
+        self.gpio.output(self.port4, False)
+
+    def three(self):
+        self.gpio.output(self.port1, True)
+        self.gpio.output(self.port2, True)
+        self.gpio.output(self.port3, False)
+        self.gpio.output(self.port4, False)
+        
+    def four(self):
+        self.gpio.output(self.port1, False)
+        self.gpio.output(self.port2, False)
+        self.gpio.output(self.port3, True)
+        self.gpio.output(self.port4, False)
+
+    def five(self):
+        self.gpio.output(self.port1, True)
+        self.gpio.output(self.port2, False)
+        self.gpio.output(self.port3, True)
+        self.gpio.output(self.port4, False)
+        
+    def six(self):
+        self.gpio.output(self.port1, False)
+        self.gpio.output(self.port2, True)
+        self.gpio.output(self.port3, True)
+        self.gpio.output(self.port4, False)
+        
+    def seven(self):
+        self.gpio.output(self.port1, True)
+        self.gpio.output(self.port2, True)
+        self.gpio.output(self.port3, True)
+        self.gpio.output(self.port4, False)
+        
+    def eight(self):
+        self.gpio.output(self.port1, False)
+        self.gpio.output(self.port2, False)
+        self.gpio.output(self.port3, False)
+        self.gpio.output(self.port4, True)
+        
+    def nine(self):
+        self.gpio.output(self.port1, True)
+        self.gpio.output(self.port2, False)
+        self.gpio.output(self.port3, False)
+        self.gpio.output(self.port4, True)
+        
+    def ten(self):
+        self.gpio.output(self.port1, False)
+        self.gpio.output(self.port2, True)
+        self.gpio.output(self.port3, False)
+        self.gpio.output(self.port4, True)
+        
+    def eleven(self):
+        self.gpio.output(self.port1, True)
+        self.gpio.output(self.port2, True)
+        self.gpio.output(self.port3, False)
+        self.gpio.output(self.port4, True)
+        
+    def twelve(self):
+        self.gpio.output(self.port1, False)
+        self.gpio.output(self.port2, False)
+        self.gpio.output(self.port3, True)
+        self.gpio.output(self.port4, True)
+        
+    def thirteen(self):
+        self.gpio.output(self.port1, True)
+        self.gpio.output(self.port2, False)
+        self.gpio.output(self.port3, True)
+        self.gpio.output(self.port4, True)
+        
+    def fourteen(self):
+        self.gpio.output(self.port1, False)
+        self.gpio.output(self.port2, True)
+        self.gpio.output(self.port3, True)
+        self.gpio.output(self.port4, True)
+        
+    def fifteen(self):
+        self.gpio.output(self.port1, True)
+        self.gpio.output(self.port2, True)
+        self.gpio.output(self.port3, True)
+        self.gpio.output(self.port4, True)
         
